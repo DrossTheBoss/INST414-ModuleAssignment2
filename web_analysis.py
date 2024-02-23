172d28596a1444d4e25726ccc5c61915815e3991
Eli Dross <Logandr2@gmail.com>
2024-02-22 19:20:27 -0500

    final script

--- web_analysis.py
+++ web_analysis.py
@@ -4,18 +4,21 @@ import pandas as pd
 
 G = nx.Graph()
 df = pd.read_csv("all_video_games(cleaned).csv")
-df = df.head(500)
+df = df.head(500) #limits the graph to 500 nodes to keep graph to a somewhat reasonable size
 
 df = df.drop("Release Date" , axis=1)
 df = df.drop("Product Rating" , axis=1)
 df = df.drop("User Score" , axis=1)
 df = df.drop("User Ratings Count" , axis=1)
+df = df.drop("Platforms Info", axis = 1)
+df = df.drop("Developer" , axis=1)
+df = df.drop("Publisher" , axis=1)
 df['Genres Splitted'] = df['Genres Splitted'].apply(lambda x: x.strip("[]").replace("'", ""))
 
 print(df.head())
 
 for index, row in df.iterrows():
-    # Split the genres by comma and remove any leading/trailing whitespaces
+    #Splits the genres by comma and remove any whitespaces
     genres = [genre.strip() for genre in row['Genres Splitted'].split(',')]
     
     # Add the game as a node to the graph
@@ -33,6 +36,7 @@ for index, row in df.iterrows():
             if weight > 0:
                 G.add_edge(row['Title'], other_row['Title'], weight=weight)
 
+# gets a list of all genres and then determines which genre shows up the most
 all_genres = [genre.strip() for sublist in df['Genres Splitted'].apply(lambda x: x.split(',')) for genre in sublist]
 most_common_genre = max(set(all_genres), key=all_genres.count)
 
@@ -42,25 +46,25 @@ print("The most common genre is:", most_common_genre)
 node_weights = {node: sum([data['weight'] for _, data in G[node].items()]) for node in G.nodes()}
 top_50_nodes = sorted(node_weights, key=node_weights.get, reverse=True)[:50]
 
-print("Top 10 nodes in terms of total edge weight:")
+print("Top 50 nodes in terms of total edge weight:")
 for i, node in enumerate(top_50_nodes, 1):
     print(f"{i}. {node}: {node_weights[node]}")
 
-# Draw the graph
+# Draws the graph
 pos = nx.spring_layout(G)  # Positions for all nodes
 
 # Draw the nodes
 nx.draw_networkx_nodes(G, pos, node_size=100, node_color='skyblue')
 
 # Draw the edges
-nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5, edge_color='gray')
+nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5, edge_color='red')
 
-# Truncate the labels to 8 characters
+# shorten the labels to 8 characters
 truncated_labels = {node: node[:8] for node in G.nodes()}
 
-# Draw the truncated labels
+# Draw the labels
 nx.draw_networkx_labels(G, pos, labels=truncated_labels, font_size=8, font_family='sans-serif')
 
 # Show the plot
-plt.axis('off')  # Turn off the axis
+plt.axis('off') 
 plt.show()
